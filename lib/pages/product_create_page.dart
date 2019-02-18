89cdd5cde5de00554284ee0cc6a067b9b46b6442
KIRPAT <osmankirpat@gmail.com>
2019-02-18 17:40:51 +0300

    forms

--- lib/pages/product_create_page.dart
+++ lib/pages/product_create_page.dart
@@ -65,79 +65,81 @@ class _ProductCreatePageState extends State<ProductCreatePage>{
     return Container(
       width: 300,
       //margin: EdgeInsets.all(10.0),
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: _targetPadding / 1.5),
-        children: <Widget>[
-          //PRODUCT NAME
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  labelText: 'ProductName' ,
+      child: Form(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: _targetPadding / 1.5),
+          children: <Widget>[
+            //PRODUCT NAME
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'ProductName' ,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    )
+                ),
+                onChanged: (String value ){
+                  setState(() {
+                    _titleValue = value;
+                  });
+                },
+              ),
+            ),
+
+            //DESCRIPTION
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Description' ,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    )
+                ),
+                maxLines: 4,
+                onChanged: (String value ){
+                  setState(() {
+                    _descriptionValue = value;
+                  });
+                },
+              ),
+            ),
+
+            //PRICE
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Price' ,
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   )
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (String value){
+                  setState(() {
+                    /*
+                    This parts needs to be improved.
+                    Write a function that checks if the input is in double format.
+                    we need try-catching
+                    */
+                    _priceValue = double.parse(value);
+                  });
+                },
               ),
-              onChanged: (String value ){
-                setState(() {
-                  _titleValue = value;
-                });
-              },
             ),
-          ),
 
-          //DESCRIPTION
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  labelText: 'Description' ,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  )
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 80),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                onPressed: _addProduct,
+                child: Text('Add Product!', style: TextStyle(color: Colors.white)),
               ),
-              maxLines: 4,
-              onChanged: (String value ){
-                setState(() {
-                  _descriptionValue = value;
-                });
-              },
             ),
-          ),
-
-          //PRICE
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Price' ,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                )
-              ),
-              keyboardType: TextInputType.number,
-              onChanged: (String value){
-                setState(() {
-                  /*
-                  This parts needs to be improved.
-                  Write a function that checks if the input is in double format.
-                  we need try-catching
-                  */
-                  _priceValue = double.parse(value);
-                });
-              },
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 80),
-            child: RaisedButton(
-              color: Theme.of(context).accentColor,
-              onPressed: _addProduct,
-              child: Text('Add Product!', style: TextStyle(color: Colors.white)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
